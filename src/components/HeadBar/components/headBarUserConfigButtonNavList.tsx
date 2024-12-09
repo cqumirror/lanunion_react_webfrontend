e635cbfb06ec785f6fbffabd927c08d0e92ac20e
INFOstudio <beta-s@outlook.com>
2024-12-10 01:05:27 +0800

    修复测试问题

--- src/components/HeadBar/components/headBarUserConfigButtonNavList.tsx
+++ src/components/HeadBar/components/headBarUserConfigButtonNavList.tsx
@@ -1,6 +1,5 @@
 // TODO 后端自动处理
 
-import { IconAbout, IconAngry, IconHouse, IconIdCard, IconMessageSquareMore, IconCalendarClock, IconWrench } from "../../../content/icon";
 import { Path } from "../../../types";
 import { Identity } from "../../../types";
 
@@ -8,12 +7,10 @@ const headBarNavList: Path[] = [
   {
     path: '/userinfo',
     name: '用户信息',
-    icon: <IconHouse />,
   },
   {
     path: '/admin',
     name: '后台',
-    icon: <IconAbout />,
   },
 ]
 
--- src/pages/RepairPage/components/formQuestionNextStepDisable.tsx
+++ src/pages/RepairPage/components/formQuestionNextStepDisable.tsx
@@ -13,19 +13,19 @@ export default function formQuestionNextStepDisable(repairFormData: RepairFormDa
           return !(
             repairFormData.faultDescription?.canBootUp !== undefined &&
             repairFormData.faultDescription?.isNoteBook !== undefined &&
-            repairFormData.faultDescription?.brandHardware !== undefined &&
-            repairFormData.faultDescription?.modelHardware !== undefined &&
-            repairFormData.faultDescription?.timeBroughtHardware !== undefined &&
-            repairFormData.faultDescription?.detailHardware !== undefined
+            [undefined, ""].includes(repairFormData.faultDescription?.brandHardware) &&
+            [undefined, ""].includes(repairFormData.faultDescription?.modelHardware) &&
+            [undefined, ""].includes(repairFormData.faultDescription?.timeBroughtHardware) &&
+            [undefined, ""].includes(repairFormData.faultDescription?.detailHardware) !== undefined
           );
         case 1:
           return !(
             repairFormData.faultDescription?.os !== undefined &&
             repairFormData.faultDescription?.osBitNumber !== undefined &&
-            repairFormData.faultDescription?.brandSoftware !== undefined &&
-            repairFormData.faultDescription?.modelSoftware !== undefined &&
-            repairFormData.faultDescription?.timeBroughtSoftware !== undefined &&
-            repairFormData.faultDescription?.detailSoftware !== undefined
+            [undefined, ""].includes(repairFormData.faultDescription?.brandSoftware) !== undefined &&
+            [undefined, ""].includes(repairFormData.faultDescription?.modelSoftware) !== undefined &&
+            [undefined, ""].includes(repairFormData.faultDescription?.timeBroughtSoftware) !== undefined &&
+            [undefined, ""].includes(repairFormData.faultDescription?.detailSoftware) !== undefined
           );
         case 2:
           return true;
@@ -35,7 +35,7 @@ export default function formQuestionNextStepDisable(repairFormData: RepairFormDa
     case 3:
       return !(
         repairFormData.contactInfo?.campus !== undefined &&
-        repairFormData.contactInfo?.addr !== undefined
+        [undefined, ""].includes(repairFormData.contactInfo?.addr)
       );
     case 4:
       return false;
